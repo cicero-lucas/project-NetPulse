88475ce75192d0f575b9b075d3854f94bc969971
cicero lucas <cicerolucassilva10@gmail.com>
2026-05-21 17:54:28 -0300

    chore(frontend): add nextjs project configuration, tailwind and dockerfile

---
+++ frontend/jest.config.js
@@ -0,0 +1,11 @@
+const nextJest = require('next/jest');
+const createJestConfig = nextJest({ dir: './' });
+
+const customJestConfig = {
+  setupFilesAfterEnv: ['<rootDir>/jest.setup.js'],
+  testEnvironment: 'jest-environment-jsdom',
+  moduleNameMapper: { '^@/(.*)$': '<rootDir>/src/$1' },
+  testMatch: ['**/__tests__/**/*.test.{ts,tsx}'],
+};
+
+module.exports = createJestConfig(customJestConfig);
